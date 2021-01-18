743e22d52881dfc4d92eee3b1bbf7ca0b182d540
18127003 <18127003@users.noreply.github.com>
2021-01-18 18:16:40 +0700

    cors fix 2

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const session = require("express-session");
 const path = require("path");
 const app = express();
 const db = require("./config/key").mongoURI;
-const { EventEmitter } = require("events");
+const cors = require("cors");
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use(
   session({
     secret: "secret",
@@ -17,13 +18,13 @@ app.use(
     saveUninitialized: true,
   })
 );
-let allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-}
-app.use(allowCrossDomain);
+// let allowCrossDomain = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// }
+// app.use(allowCrossDomain);
 app.use("/", require("./route/api.js"));
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
